193c59d099aec2be7bbd87cb0180cc0ca53de164
Mo2024 <mohdosama2025@gmail.com>
2022-01-27 05:09:52 +0300

    Creating products

--- index.js
+++ index.js
@@ -14,11 +14,23 @@ mongoose.connect('mongodb://localhost:27017/farmdb', { useNewUrlParser: true })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
 
 app.get('/products', async (req, res) => {
 
     const products = await Product.find({})
     res.render('products/index', { products })
+
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
 
 })
 
