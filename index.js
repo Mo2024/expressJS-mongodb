cd0efa1be720c1e14b6f3857c233f17adc5810de
Mo2024 <mohdosama2025@gmail.com>
2022-01-30 02:45:11 +0300

    Category selector fixed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const Product = require('./models/product');
 const methodOverride = require('method-override')
 
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 mongoose.connect('mongodb://localhost:27017/farmdb', { useNewUrlParser: true })
     .then(() => {
@@ -21,7 +22,7 @@ app.use(methodOverride('_method'))
 
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 
 
@@ -42,7 +43,6 @@ app.post('/products', async (req, res) => {
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
-    console.log(product)
     res.render('products/show', { product })
 })
 
@@ -55,7 +55,7 @@ app.put('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id);
-    res.render('products/edit', { product })
+    res.render('products/edit', { product, categories })
 })
 app.listen(3000, () => {
     console.log("Listening to port 3000")
