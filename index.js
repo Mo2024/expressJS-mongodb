8c5d9bf00771054c2db347de359f82a29905a3e1
Mo2024 <mohdosama2025@gmail.com>
2022-01-29 19:06:43 +0300

    updating existing data

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
+const methodOverride = require('method-override')
+
 
 mongoose.connect('mongodb://localhost:27017/farmdb', { useNewUrlParser: true })
     .then(() => {
@@ -15,10 +17,13 @@ mongoose.connect('mongodb://localhost:27017/farmdb', { useNewUrlParser: true })
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 app.get('/products/new', (req, res) => {
     res.render('products/new')
 })
+
 
 app.get('/products', async (req, res) => {
 
@@ -41,6 +46,19 @@ app.get('/products/:id', async (req, res) => {
     res.render('products/show', { product })
 })
 
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    Product.findByIdAndUpdate(id, req.body)
+
+    console.log(req.body)
+    res.send('PUT')
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id);
+    res.render('products/edit', { product })
+})
 app.listen(3000, () => {
     console.log("Listening to port 3000")
 })
